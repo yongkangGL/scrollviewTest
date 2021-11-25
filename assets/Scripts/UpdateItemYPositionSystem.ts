b6cf4f295b059c1dff09d4f03af5d8526bc40cba
Joel <simkalok0914@gmail.com>
2021-11-25 13:58:09 +0800

    Fixed the order of the item not loaded correctly

--- assets/Scripts/UpdateItemYPositionSystem.ts
+++ assets/Scripts/UpdateItemYPositionSystem.ts
@@ -15,34 +15,14 @@ export default class UpdateItemYPositionSystem extends cc.Component {
         let globalPosition = this.getGlobalPosition();
         if(globalPosition.y >= this.scrollViewComponent.topBorder){ // reached top border
             this.node.y -= (this.scrollViewComponent.topBorder - this.scrollViewComponent.bottomSpawn);
-            let indexOfHighestContentIndex = this.getIndexOfItemArrayWhereHighestContentIndex();
-            this.itemComponent.contentIndex = this.scrollViewComponent.itemArray[indexOfHighestContentIndex].contentIndex + 1;
+            this.itemComponent.contentIndex += this.scrollViewComponent.itemArray.length;
         }
         else if(globalPosition.y <= this.scrollViewComponent.bottomBorder){ // reached bottom border
             this.node.y += (this.scrollViewComponent.topSpawn - this.scrollViewComponent.bottomBorder);
-            let indexOfLowestContentIndex = this.getIndexOfItemArrayWhereLowestContentIndex();
-            this.itemComponent.contentIndex = this.scrollViewComponent.itemArray[indexOfLowestContentIndex].contentIndex - 1;
+            this.itemComponent.contentIndex -= this.scrollViewComponent.itemArray.length;
         }
     }
     getGlobalPosition(){
         return GlobalComponent.instance.canvasNode.convertToNodeSpaceAR(this.node.parent.convertToWorldSpaceAR(this.node.position));
-    }
-    getIndexOfItemArrayWhereHighestContentIndex(){
-        let index = 0;
-        for (let i = 0; i < this.scrollViewComponent.itemArray.length; i++){
-            if(this.scrollViewComponent.itemArray[i].contentIndex > this.scrollViewComponent.itemArray[index].contentIndex){
-                index = i;
-            }
-        }
-        return index;
-    }
-    getIndexOfItemArrayWhereLowestContentIndex(){
-        let index = 0;
-        for (let i = 0; i < this.scrollViewComponent.itemArray.length; i++){
-            if(this.scrollViewComponent.itemArray[i].contentIndex < this.scrollViewComponent.itemArray[index].contentIndex){
-                index = i;
-            }
-        }
-        return index;
     }
 }
